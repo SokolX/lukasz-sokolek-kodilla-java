455d91dac06f02a35e95509835810e78f9c617cc
sokolx <89soek@gmail.com>
2022-11-10 15:18:21 +0100

    6.3.6-7 Added @BeforeEach and @AfterEach, change to Arrys.asList() in test CollectionTestSuite

--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -1,20 +1,37 @@
 package com.kodilla.testing.collection;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 @DisplayName("Collection Test Suite")
 public class CollectionTestSuite {
 
+    private TestInfo testInfo;
+
+    @BeforeEach
+    void init(TestInfo testInfo) {
+        this.testInfo = testInfo;
+    }
+    @BeforeEach
+    public void before() {
+        System.out.println("Test Case: begin");
+    }
+
+    @AfterEach
+    public void after() {
+        System.out.println("Test Case: end");
+    }
+
     @DisplayName("when list number is empty, " +
             "then testOddNumbersExterminatorNormalList should return empty list"
     )
     @Test
     void testOddNumbersExterminatorEmptyList() {
+        System.out.println(testInfo.getDisplayName());
         //given
         //List<Integer> emptyList = ;
         OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
@@ -30,11 +47,12 @@ public class CollectionTestSuite {
     )
     @Test
     void testOddNumbersExterminatorNormalList() {
+        System.out.println(testInfo.getDisplayName());
         //given
         //List<Integer> emptyList = ;
         OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
         //when
-        List<Integer> exceptedList = oddNumbersExterminator.exterminate(List.of(4, 2, 7, 1, 5, 7, 1, 4, 1));
+        List<Integer> exceptedList = oddNumbersExterminator.exterminate(Arrays.asList(4, 2, 7, 1, 5, 7, 1, 4, 1));
         //then
         Assertions.assertEquals(List.of(4, 2, 4), exceptedList);
 
