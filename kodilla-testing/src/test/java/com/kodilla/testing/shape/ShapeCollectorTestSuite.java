b5a9994d2cdb4b301807f5ca0f7ab79eeec11c97
sokolx <89soek@gmail.com>
2022-11-14 16:37:01 +0100

    6.3.2 Deleted not used import List...

--- kodilla-testing/src/test/java/com/kodilla/testing/shape/ShapeCollectorTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/shape/ShapeCollectorTestSuite.java
@@ -5,8 +5,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
 @DisplayName("TDD: ShapeCollector Test Suite")
 public class ShapeCollectorTestSuite {
     @Nested
