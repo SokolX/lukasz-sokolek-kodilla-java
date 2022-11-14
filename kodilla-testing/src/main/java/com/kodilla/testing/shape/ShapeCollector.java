5a12d303c3bccb47fbe678439a65cdfcdc596438
sokolx <89soek@gmail.com>
2022-11-14 16:19:32 +0100

    6.3.1 Added test to notimplemented ShapeCollector.java

--- kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
@@ -23,9 +23,7 @@ public class ShapeCollector {
         return null;
     }
 
-    public void showFigures() {
-        for (Shape shape : shapes) {
-            System.out.println("Shape: " + shape.getShapeName() + ", area: " + shape.getArea());
-        }
+    public String showFigures() {
+        return null;
     }
 }
--- kodilla-testing/src/test/java/com/kodilla/testing/shape/ShapeCollectorTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/shape/ShapeCollectorTestSuite.java
@@ -5,14 +5,31 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 @DisplayName("TDD: ShapeCollector Test Suite")
 public class ShapeCollectorTestSuite {
-
     @Nested
-    @DisplayName("Tests for adding figures")
-    class AddFigures {
+    @DisplayName("Tests for update list figures in shape collector")
+    class UpdateCollectorFigures {
         @Test
-        void shouldGetShapesWhenListIsNotEmpty() {
+        @DisplayName("Tests add shape to shapeCollector")
+        void shouldAddShapeToShapeCollector() {
+            //given
+            ShapeCollector shapeCollector = new ShapeCollector();
+
+            //when
+            shapeCollector.addFigure(new Square(4));
+            shapeCollector.addFigure(new Triangle(7,5,4));
+            shapeCollector.addFigure(new Circle(4));
+
+            //then
+            Assertions.assertEquals(3, shapeCollector.getShapes().size());
+        }
+
+        @Test
+        @DisplayName("Tests remove shape to shapeCollector")
+        void shouldRemoveShapeToShapeCollector() {
             //given
             ShapeCollector shapeCollector = new ShapeCollector();
             Shape square = new Square(4);
@@ -22,19 +39,46 @@ public class ShapeCollectorTestSuite {
             shapeCollector.addFigure(square);
             shapeCollector.addFigure(triangle);
             shapeCollector.addFigure(circle);
+            shapeCollector.removeFigure(circle);
             //then
-            Assertions.assertEquals(3, shapeCollector.getShapes().size());
+            Assertions.assertEquals(2, shapeCollector.getShapes().size());
         }
+    }
 
+    @Nested
+    @DisplayName("Tests for get list figures in shape collector")
+    class GetCollectorFigures {
         @Test
-        void shouldGetShapesWhenListIsEmpty() {
+        @DisplayName("Tests get shapes from shapeCollector")
+        void shouldGetShapeFromShapeCollector() {
             //given
             ShapeCollector shapeCollector = new ShapeCollector();
 
             //when
+            shapeCollector.addFigure(new Square(4));
+            shapeCollector.addFigure(new Triangle(7,5,4));
+            shapeCollector.addFigure(new Circle(4));
+            Shape shape = shapeCollector.getFigure(1);
 
             //then
-            Assertions.assertEquals(0, shapeCollector.getShapes().size());
+            Assertions.assertEquals("Triangle", shape.getShapeName());
+        }
+
+        @Test
+        @DisplayName("Tests remove shape to shapeCollector")
+        void shouldShowShapeFromShapeCollector() {
+            //given
+            ShapeCollector shapeCollector = new ShapeCollector();
+
+            //when
+            shapeCollector.addFigure(new Square(4));
+            shapeCollector.addFigure(new Triangle(7,5,4));
+            shapeCollector.addFigure(new Circle(4));
+            String figures = shapeCollector.showFigures();
+
+            //then
+            Assertions.assertNotNull(figures);
         }
     }
 }
+
