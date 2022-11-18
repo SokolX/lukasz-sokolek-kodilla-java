dae1e9653c58bd71c767ee749bc6c34f36173f7e
sokolx <89soek@gmail.com>
2022-11-18 14:59:02 +0100

    7.1.1. Added example lambda code

---
+++ kodilla-stream/src/main/java/com/kodilla/StreamMain.java
@@ -0,0 +1,26 @@
+package com.kodilla;
+
+import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.lambda.FunctionalCalculator;
+import com.kodilla.stream.lambda.Processor;
+
+public class StreamMain {
+    public static void main(String[] args) {
+        Processor processor = new Processor();
+        processor.execute(() -> System.out.println("This is an example text."));
+
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        System.out.println("Calculating expressions with method references");
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+    }
+}
