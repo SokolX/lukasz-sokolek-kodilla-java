ffa5ac8187cf6f64ac4d18cc81a59fc3c6c2b3c0
sokolx <89soek@gmail.com>
2022-11-20 13:39:21 +0100

    7.3.2. added forumuser and forum, use in StreamMain.java

--- kodilla-stream/src/main/java/com/kodilla/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/StreamMain.java
@@ -3,12 +3,15 @@ package com.kodilla;
 import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.book.Book;
 import com.kodilla.stream.book.BookDirectory;
+import com.kodilla.stream.forumuser.Forum;
+import com.kodilla.stream.forumuser.ForumUser;
 import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.lambda.FunctionalCalculator;
 import com.kodilla.stream.lambda.Processor;
 import com.kodilla.stream.person.People;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -69,11 +72,24 @@ public class StreamMain {
                 .forEach(System.out::println);
 
         BookDirectory theBookDirectoryJoining = new BookDirectory();
-        String theResultStringOfBooksJoining = theBookDirectory.getList().stream()  // [1]
+        String theResultStringOfBooksJoining = theBookDirectory.getList().stream()
                 .filter(book -> book.getYearOfPublication() > 2005)
                 .map(Book::toString)
-                .collect(Collectors.joining(",\n","<<",">>"));                    // [2]
+                .collect(Collectors.joining(",\n","<<",">>"));
 
         System.out.println(theResultStringOfBooksJoining);
+
+
+        Forum forumUsers = new Forum();
+
+        Map<Integer, ForumUser> users = forumUsers.getUserList().stream()
+                .filter(user -> user.getSex() == 'M')
+                .filter(user -> user.getDateOfBirth().isBefore(LocalDate.now().minusYears(20)))
+                .filter(user -> user.getCounterOfPublishedPosts() >= 1)
+                .collect(Collectors.toMap(ForumUser::getId, user -> user));
+
+        for (Map.Entry<Integer, ForumUser> entry : users.entrySet()) {
+            System.out.println(entry.getKey() + " = " + entry.getValue());
+        }
     }
 }
