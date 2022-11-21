90d674f4fdb0384f9c1294aec5da64bea203ddbf
sokolx <89soek@gmail.com>
2022-11-21 21:24:53 +0100

    7.3.2. Refacot forumuser and forum

--- kodilla-stream/src/main/java/com/kodilla/stream/forumuser/ForumUser.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/forumuser/ForumUser.java
@@ -2,41 +2,7 @@ package com.kodilla.stream.forumuser;
 
 import java.time.LocalDate;
 
-public final class ForumUser {
-
-    private final int id;
-    private final String name;
-    private final char sex;
-    private final LocalDate dateOfBirth;
-    private final int counterOfPublishedPosts;
-
-    public ForumUser(int id, String name, char sex, LocalDate dateOfBirth, int counterOfPublishedPosts) {
-        this.id = id;
-        this.name = name;
-        this.sex = sex;
-        this.dateOfBirth = dateOfBirth;
-        this.counterOfPublishedPosts = counterOfPublishedPosts;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public char getSex() {
-        return sex;
-    }
-
-    public LocalDate getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public int getCounterOfPublishedPosts() {
-        return counterOfPublishedPosts;
-    }
+public record ForumUser(int id, String name, char sex, LocalDate dateOfBirth, int counterOfPublishedPosts) {
 
     @Override
     public String toString() {
