d14a9a050e4cf3ff8187fb04691e9fb473afaf2b
sokolx <89soek@gmail.com>
2022-11-24 08:55:22 +0100

    7.4.3. Changed Continent.java, Country.java to Record

--- kodilla-stream/src/main/java/com/kodilla/stream/world/Continent.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/world/Continent.java
@@ -2,19 +2,7 @@ package com.kodilla.stream.world;
 
 import java.util.List;
 
-public final class Continent {
-
-    private final String continentName;
-    private final List<Country> countries;
-
-    public Continent(final String continentName, final List<Country> countries) {
-        this.continentName = continentName;
-        this.countries = countries;
-    }
-
-    public String getContinentName() {
-        return continentName;
-    }
+public record Continent(String continentName, List<Country> countries) {
 
     public List<Country> getCountries() {
         return countries;
--- kodilla-stream/src/main/java/com/kodilla/stream/world/Country.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/world/Country.java
@@ -2,19 +2,7 @@ package com.kodilla.stream.world;
 
 import java.math.BigDecimal;
 
-public final class Country {
-
-    private final String nameCountry;
-    private final BigDecimal peopleQuantity;
-
-    public Country(final String nameCountry, final BigDecimal peopleQuantity) {
-        this.nameCountry = nameCountry;
-        this.peopleQuantity = peopleQuantity;
-    }
-
-    public String getNameCountry() {
-        return nameCountry;
-    }
+public record Country(String nameCountry, BigDecimal peopleQuantity) {
 
     public BigDecimal getPeopleQuantity() {
         return peopleQuantity;
