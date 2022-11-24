f810e0084102df741d3c66e3e40d045115eaad50
sokolx <89soek@gmail.com>
2022-11-24 20:43:42 +0100

    7.5.3. Fix change of to range and add map :)

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -1,15 +1,18 @@
 package com.kodilla.stream.array;
 
+import java.util.Arrays;
 import java.util.stream.IntStream;
 
 public interface ArrayOperations {
 
     static double getAverage(int[] numbers) {
 
-        IntStream.of(numbers)
+        IntStream.range(0, numbers.length)
+                .map(i -> numbers[i])
                 .forEach(System.out::println);
 
-        return IntStream.of(numbers)
+        return IntStream.range(0, numbers.length)
+                .map(i -> numbers[i])
                 .average()
                 .orElse(0.0);
     }
