d7f0e56c9e4499837bead3626210b43f65a0cad9
sokolx <89soek@gmail.com>
2022-11-29 17:32:41 +0100

    7.5.3. change IntStream to Arrays.stream

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -1,13 +1,13 @@
 package com.kodilla.stream.array;
 
+import java.util.Arrays;
 import java.util.stream.IntStream;
 
 public interface ArrayOperations {
 
     static double getAverage(int[] numbers) {
 
-        IntStream.range(0, numbers.length)
-                .map(i -> numbers[i])
+        Arrays.stream(numbers, 0, numbers.length)
                 .forEach(System.out::println);
 
         return IntStream.range(0, numbers.length)
