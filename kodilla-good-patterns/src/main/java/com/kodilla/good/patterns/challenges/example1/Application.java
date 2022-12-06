40573133c69e814bf118677932b48f757c6cdfa0
sokolx <89soek@gmail.com>
2022-12-06 13:43:38 +0100

    9.2.1 Refactor example 1

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/example1/Application.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/example1/Application.java
@@ -1,10 +1,4 @@
-package com.kodilla.good.patterns.challanges;
-
-import com.kodilla.good.patterns.challanges.example.CarRentalRepository;
-import com.kodilla.good.patterns.challanges.example.CarRentalService;
-import com.kodilla.good.patterns.challanges.example.MailService;
-import com.kodilla.good.patterns.challanges.example.User;
-import com.kodilla.good.patterns.challanges.task1.*;
+package com.kodilla.good.patterns.challenges.example1;
 
 import java.time.LocalDateTime;
 
@@ -12,7 +6,6 @@ public class Application {
 
     public static void main(String[] args) {
 
-        System.out.println("---> EXAMPLE <---");
         User user = new User("Test", "Test", "test@test.pl");
 
         LocalDateTime rentFrom = LocalDateTime.of(2017, 8, 1, 12, 0);
@@ -30,14 +23,5 @@ public class Application {
         } else {
             System.out.println("Car rent has been rejected!");
         }
-
-        System.out.println("\n---> TASK1 <---");
-        Product product = new Product(1, "T-Shirt", 59.99);
-        OrderRequest orderRequest = new OrderRequest(user, LocalDateTime.now(), product);
-
-        OrderProcessor orderProcessor = new OrderProcessor(new MailService(), new ProductOrderService(), new OrderPaymentRepository());
-        orderProcessor.process(orderRequest);
-
-
     }
 }
