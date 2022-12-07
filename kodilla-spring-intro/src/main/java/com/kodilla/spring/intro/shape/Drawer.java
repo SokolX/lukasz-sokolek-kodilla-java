2930782e595af24580ab2a23b19cfcb6ee38b100
sokolx <89soek@gmail.com>
2022-12-07 08:27:40 +0100

    10.1. Refactor impl Drawer

--- kodilla-spring-intro/src/main/java/com/kodilla/spring/intro/shape/Drawer.java
+++ kodilla-spring-intro/src/main/java/com/kodilla/spring/intro/shape/Drawer.java
@@ -2,12 +2,13 @@ package com.kodilla.spring.intro.shape;
 
 public class Drawer {
 
-    public void doDrawing() {
-        Shape shape;
-        shape = new Circle();
-        shape.draw();
+    final Shape shape;
 
-        shape = new Triangle();
-        shape.draw();
+    public Drawer(final Shape shape) {
+        this.shape = shape;
+    }
+
+    public String doDrawing() {
+        return shape.draw();
     }
 }
