1881df33df1d6d18cc8d522ce636f30b4abf5a26
sokolx <89soek@gmail.com>
2022-12-07 13:34:10 +0100

    10.1. Moved shape to package

--- kodilla-spring/src/test/java/com/kodilla/spring/KodillaSpringApplicationTests.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/KodillaSpringApplicationTests.java
@@ -1,5 +1,8 @@
 package com.kodilla.spring;
 
+import com.kodilla.spring.shape.Circle;
+import com.kodilla.spring.shape.Shape;
+import com.kodilla.spring.shape.Triangle;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
