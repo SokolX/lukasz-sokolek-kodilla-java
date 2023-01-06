1aa6a98fdb88c2d08d16c475f94f043701e8a29b
sokolx <89soek@gmail.com>
2023-01-06 20:25:14 +0100

    13.3.3. fix test: deleted assertNotNull, add test to method deletedAll

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/invoice/dao/InvoiceDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/invoice/dao/InvoiceDaoTestSuite.java
@@ -3,11 +3,13 @@ package com.kodilla.hibernate.invoice.dao;
 import com.kodilla.hibernate.invoice.Invoice;
 import com.kodilla.hibernate.invoice.Item;
 import com.kodilla.hibernate.invoice.Product;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,6 +19,16 @@ class InvoiceDaoTestSuite {
     @Autowired
     private InvoiceDao invoiceDao;
 
+    @Autowired
+    private ProductDao productDao;
+
+    @Autowired
+    private ItemDao itemDao;
+
+    @AfterEach
+    void cleanUp() {
+        invoiceDao.deleteAll();
+    }
     @Test
     void testInvoiceDaoSave() {
         //given
@@ -32,14 +44,33 @@ class InvoiceDaoTestSuite {
 
         //when
         invoiceDao.save(invoice);
-        int idInvoice = invoice.getId();
         long sum = invoiceDao.count();
 
         //then
-        assertNotNull(idInvoice);
         assertEquals(1L, sum);
+    }
 
-        //cleanup
-        invoiceDao.deleteById(idInvoice);
+    @Test
+    void testDeleteAllInvoiceWithProductAndItems() {
+        //given
+        Product milk = new Product("Milk");
+        Product butter = new Product("Butter");
+        Item item1 = new Item(milk, new BigDecimal(4.99), 1, new BigDecimal(4.99));
+        Item item2 = new Item(butter, new BigDecimal(4.99), 1, new BigDecimal(4.99));
+        Invoice invoice = new Invoice("F1/01/2022");
+        invoice.getItems().add(item1);
+        invoice.getItems().add(item2);
+        item1.setInvoice(invoice);
+        item2.setInvoice(invoice);
+
+        //when
+        invoiceDao.save(invoice);
+        invoiceDao.deleteAll();
+        List<Product> products = (List<Product>) productDao.findAll();
+        List<Item> items = (List<Item>) itemDao.findAll();
+
+        //then
+        assertEquals(0, products.size());
+        assertEquals(0, items.size());
     }
 }
